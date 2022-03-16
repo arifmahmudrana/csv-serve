4f5beb58510ecf7c46dcec7f6772a3221815935c
Arif Mahmud Rana <arif@easyatwork.com>
2022-03-16 19:56:32 +0600

    redis middleware serve promotion

--- cmd/middleware.go
+++ cmd/middleware.go
@@ -38,3 +38,41 @@ func (app *application) Memcache(next http.Handler) http.Handler {
 		next.ServeHTTP(w, r)
 	})
 }
+
+func (app *application) Redis(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		app.infoLog.Println("Redis: Running middleware")
+
+		if id := chi.URLParam(r, "promotionID"); id != "" {
+			var res Promotion
+			found, err := app.r.Get(id, &res)
+
+			if err != nil {
+				app.errorLog.Println(err)
+				w.WriteHeader(http.StatusInternalServerError)
+				w.Write([]byte("Something went wrong"))
+				return
+			}
+
+			if found {
+				app.infoLog.Println("Redis: found serving from cache ", id)
+				encoder := json.NewEncoder(w)
+				if err := encoder.Encode(res); err != nil {
+					app.errorLog.Println(err)
+				}
+
+				go func(res Promotion) {
+					if err := app.m.Set(res.ID, res); err != nil {
+						app.errorLog.Println(err)
+					}
+				}(res)
+
+				return
+			}
+
+			app.infoLog.Println("Redis: miss ", id)
+		}
+
+		next.ServeHTTP(w, r)
+	})
+}
